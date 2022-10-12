a17b75cd9e1240db5792bf4ffc88378925f06e4b
James Hodgkinson <james@terminaloutcomes.com>
2022-10-12 22:25:34 +1000

    using auth creds now

--- ldapsearch_json/__init__.py
+++ ldapsearch_json/__init__.py
@@ -2,9 +2,10 @@
 
 import json
 import ssl
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 from ldap3 import ALL, Connection, Server, Tls
+from ldap3.core.exceptions import LDAPBindError
 from loguru import logger
 import typer
 
@@ -29,11 +30,14 @@ def cleandict(data: Dict[str, Any]) -> Dict[str,Any]:
             retval[key] = data[key]
     return retval
 
+#pylint: disable=too-many-arguments
 def cli(
     server: str,
     base: str,
     filter_string: str = typer.Option(FILTER, "--filter", "-f"),
     search_attributes: List[str] = typer.Option(SEARCH_ATTRIBUTES_DEFAULT, "--search-attributes", "-s" ),
+    username: Optional[str]=None,
+    password: Optional[str]=None,
     ) -> None:
     """ JSON-dumping terrible knockoff of ldapsearch """
     if not search_attributes:
@@ -41,14 +45,17 @@ def cli(
 
     tls_configuration = Tls(validate=ssl.CERT_REQUIRED, version=ssl.PROTOCOL_TLSv1_2)
     ldap_server = Server(server, use_ssl=True, get_info=ALL, tls=tls_configuration)
-    with Connection(ldap_server) as conn:
-        logger.debug("Doing search on base {} filter {} attributes {}", base, filter_string, search_attributes)
-        conn.search(search_base=base, search_filter=filter_string, attributes=search_attributes)
+    try:
+        with Connection(ldap_server,user=username, password=password) as conn:
+            logger.debug("Doing search on base {} filter {} attributes {}", base, filter_string, search_attributes)
+            conn.search(search_base=base, search_filter=filter_string, attributes=search_attributes)
 
-        for entry in conn.entries:
-            try:
-                data = json.loads(entry.entry_to_json())
-            except json.JSONDecodeError as json_error:
-                logger.error("Failed to parse JSON for entry {}: {}", entry, json_error)
-            cleaned_data = cleandict(data)
-            print(json.dumps(cleaned_data, ensure_ascii=False))
+            for entry in conn.entries:
+                try:
+                    data = json.loads(entry.entry_to_json())
+                except json.JSONDecodeError as json_error:
+                    logger.error("Failed to parse JSON for entry {}: {}", entry, json_error)
+                cleaned_data = cleandict(data)
+                print(json.dumps(cleaned_data, ensure_ascii=False))
+    except LDAPBindError as error:
+        logger.error("Failed to connect: {}", error)
